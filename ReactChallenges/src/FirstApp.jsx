bd290dd89703d61131551ba2ed2b23dd4471c2eb
Felipe-Aristizabal <andres.aristizabal_m@uao.edu.co>
2023-02-13 20:23:40 -0500

    Archivos que contienen el challenge 3 completado.

--- ReactChallenges/src/FirstApp.jsx
+++ ReactChallenges/src/FirstApp.jsx
@@ -1,14 +1,34 @@
 //rafce para la "Magia"
-//Challenge #2
-
+//Challenge #3
+import { useState } from 'react'
 import React from 'react'
+import "./challenge.css"
 
-const FirstApp = () => {
-  return (
-    <>
-    <h1> title </h1>
-    <span> 10 </span>
-    </>
+const FirstApp = ({ value }) => {
+
+    const [counter, setCounter] = useState(0)
+
+    const handleAdd = () => {
+        setCounter( counter + 1)
+    }
+
+    const handleSubsstract = () => {
+        setCounter( counter - 1)
+    }
+
+    const handleReset = () => {
+        setCounter( 0 )
+    }
+
+    return (
+        <>
+            <h1> Counter </h1>
+            <span> El contador está en: { counter } </span>
+            <hr></hr>
+            <button className='botones' onClick = { () => handleAdd() }> +1 </button>
+            <button className='botones' onClick = { () => handleSubsstract() }> -1 </button>
+            <button className='botones'  onClick = { () => handleReset() }> Reset </button>
+        </>
   )
 }
 
