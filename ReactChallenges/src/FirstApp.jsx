977ca1f7f25fa7df0775209bbd1273e73b1d5cfd
Felipe-Aristizabal <andres.aristizabal_m@uao.edu.co>
2023-02-13 19:40:30 -0500

    Archivos que contienen el challenge 2 completado

---
+++ ReactChallenges/src/FirstApp.jsx
@@ -0,0 +1,15 @@
+//rafce para la "Magia"
+//Challenge #2
+
+import React from 'react'
+
+const FirstApp = () => {
+  return (
+    <>
+    <h1> title </h1>
+    <span> 10 </span>
+    </>
+  )
+}
+
+export default FirstApp
