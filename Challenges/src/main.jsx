01b4684f05a692362f232ae89477ad399110fbc9
Felipe-Aristizabal <andres.aristizabal_m@uao.edu.co>
2023-03-17 13:44:53 -0500

    Archivos que contienen la solución del challenge 9

---
+++ Challenges/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Father } from "./Challenge9/Father";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<>
+		<Father />
+	</>
+);
