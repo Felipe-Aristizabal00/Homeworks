58f237548807cf15edd098a948b5373cd80bc249
Felipe-Aristizabal <andres.aristizabal_m@uao.edu.co>
2023-05-25 01:38:42 -0500

    Archivos que contienen la solución del challenge19

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,83 @@
 const express = require('express');
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const Usuario = require('../models/UsuarioScheme');
+const { generarJWT } = require('../helpers/jwt')
 
-const crearUsuario = (req, res = express.request ) => {
+const crearUsuario = async (req, res = express.request ) => {
     const { name, email, password } = req.body
 
-    const errors = validationResult( req );
-    if (!errors.isEmpty() ){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
+    try{
+        let usuario = await Usuario.findOne({ email: email })
+        if ( usuario ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario con ese correo ya existe en la DB'
+            })
+        }
+
+        usuario = new Usuario( req.body )
+        const salt = bcrypt.genSaltSync()
+        usuario.password = bcrypt.hashSync(password, salt)
+        await usuario.save()
+
+        res.status(200).json({
+            ok: true,
+            usuario
+        })
+
+    }catch(error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false, 
+            error,
         })
     }
-
-    res.status(200).json({
-        ok: true, 
-        name, email, password
-    })
 }
 
-const loginUsuario = (req, res = express.request) => {
-    res.json({
-        ok: true
-    })
+const loginUsuario = async (req, res = express.request) => {
+    const { email, password } = req.body
+
+    try{
+        let usuario = await Usuario.findOne({ email: email })
+        if ( !usuario ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario NO existe en la DB'
+            })
+        }
+
+        const passwordValid = bcrypt.compareSync(password, usuario.password)
+        if ( !passwordValid ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El password NO es válido'
+            })
+        }
+
+        const token = await( generarJWT(usuario.id, usuario.name))
+
+        res.status(200).json({
+            ok: true,
+            usuario,
+            token
+        })
+
+    }catch(error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false, 
+            error,
+        })
+    }
 }
 
 const revalidarToken = (req, res = express.request) => {
+
+    const { uid, name } = req
+
+    const token = await( generarJWT(uid, name))
+
     res.json({
         ok:true
     })
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator');
-const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
-const { validarCampos } = require('../middlewares/validar-campos');
+const { check } = require('express-validator') 
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth.js')
+const { listarUsuarios } = require('../controllers/users.js')
+const { validarCampos } = require ('../middlewares/validar-campos.js');
+const { validarJWT } = require('../middlewares/validar-token.js');
 
 router.post('/', loginUsuario)
 
@@ -10,11 +12,13 @@ router.post(
     '/new',
     [
         check('name', 'El nombre es obligatorio').not().isEmpty(),
-        check('email','El email es obligatorio').isEmail(),
-        check('password', ).isLength({min:6}),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La clave debe tener al menos 6 dígitos').isLength({ min: 6 }),
         validarCampos
     ],
     crearUsuario)
-router.get('/renew', revalidarToken )
+    
+router.get('/renew', validarJWT, revalidarToken)
+router.get('/list', listarUsuarios)
 
 module.exports = router;
